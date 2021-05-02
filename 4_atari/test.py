4ed277b0d890da202fe6807cb1766118dbccb0c2
okhobian <okhobian@gmail.com>
2021-05-02 13:01:45 -0500

    update

--- 4_atari/test.py
+++ 4_atari/test.py
@@ -1,30 +1,7 @@
 import gym
-import random
-
-env = gym.make('Breakout-v0')
-height, width, channels = env.observation_space.shape
-actions = env.action_space.n
-
-action = env.unwrapped.get_action_meanings()
-print(action)
-print(height, width, channels)
-
-# episodes = 5
-# for episode in range(1, episodes+1):
-#     state = env.reset()
-#     done = False
-#     score = 0
-    
-#     while not done:
-#         env.render()
-#         action = random.choice([0,1,2,3])
-#         n_state, reward, done, info = env.step(action)
-#         score+=reward
-#     print('Episode:{} Score"{}'.format(episode,score))
-# env.close()
-
-
+import cv2
 import numpy as np
+
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Convolution2D
 from tensorflow.keras.optimizers import Adam
@@ -33,30 +10,35 @@ from tensorflow.keras.callbacks import CSVLogger
 from rl.agents import DQNAgent
 from rl.memory import SequentialMemory
 from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy
-from rl.callbacks import ModelIntervalCheckpoint
+from rl.callbacks import ModelIntervalCheckpoint, FileLogger
 from rl.core import Processor
 
-import cv2
-
-# import os
+import os
 # os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
+ENV_NAME = 'Breakout-v0'
+NUM_STEPS = 10000
 
-def build_model(height,width,channels,actions):
-    model = Sequential()
-    # shape = (3, height, width, channels)
-    shape = (3, 210, 160, 1)
-    model.add(Convolution2D(32, (8,8), strides=(4,4), activation='relu', input_shape=shape))
-    model.add(Convolution2D(64, (4,4), strides=(2,2), activation='relu'))
-    model.add(Convolution2D(64, (3,3), activation='relu'))
-    model.add(Flatten())
-    model.add(Dense(512, activation='relu'))
-    model.add(Dense(256, activation='relu'))
-    model.add(Dense(actions, activation='linear'))
-    return model
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+INSTANCE_PATH = os.path.join(DIR_PATH, 'logs', '{}_steps'.format(NUM_STEPS))
+CHECKPOINTS_PATH = os.path.join(INSTANCE_PATH, 'checkpoints')
 
-model = build_model(height,width,channels,actions)
-model.summary()
+# print(INSTANCE_PATH)
+if not os.path.exists(INSTANCE_PATH):
+    os.makedirs(INSTANCE_PATH)
+    
+if not os.path.exists(CHECKPOINTS_PATH):
+    os.makedirs(CHECKPOINTS_PATH)
+
+env = gym.make(ENV_NAME)
+height, width, channels = env.observation_space.shape
+actions = env.action_space.n
+action = env.unwrapped.get_action_meanings()
+print(action)
+print(height, width, channels)
+
+
+
 
 class CustomerProcessor(Processor):      
     def process_step(self, observation, reward, done, info):
@@ -74,13 +56,37 @@ class CustomerProcessor(Processor):
     def process_reward(self, reward):
         return reward
 
+def build_model(height, width, actions):
+    shape = (3, height, width, 1)
+    # shape = (3, 210, 160, 1)
+    model = Sequential()
+    model.add(Convolution2D(32, (8,8), strides=(4,4), activation='relu', input_shape=shape))
+    model.add(Convolution2D(64, (4,4), strides=(2,2), activation='relu'))
+    model.add(Convolution2D(64, (3,3), activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(actions, activation='linear'))
+    return model
+
+model = build_model(height, width, actions)
+model.summary()
+
+def build_callbacks(env_name):
+    # checkpoint_weights_filename = CHECKPOINTS_PATH + '\\dqn_' + env_name + '_weights_{step}.h5f'
+    log_filename = INSTANCE_PATH + '\\dqn_{}_log.json'.format(env_name)
+    # callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=1000)]
+    callbacks += [FileLogger(log_filename, interval=100)]
+    return callbacks
+
 def build_agent(model, actions):
     policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), 
                                   attr='eps', 
                                   value_max=1., 
                                   value_min=.1, 
                                   value_test=.2, 
-                                  nb_steps=10000)
+                                  nb_steps=NUM_STEPS
+    )
     memory = SequentialMemory(limit=1000, window_length=3)
     dqn = DQNAgent(model=model, 
                    processor=CustomerProcessor(), 
@@ -88,14 +94,23 @@ def build_agent(model, actions):
                    enable_dueling_network=True, 
                    dueling_type='avg', 
                    nb_actions=actions, 
-                   nb_steps_warmup=1000
-                   )
+                   nb_steps_warmup=50
+    )
     return dqn
 
-dqn = build_agent(model, actions)
-dqn.compile(Adam(lr=1e-4))
-# csv_logger = CSVLogger('training_log.csv', separator=',', append=False)
-# dqn.fit(env, nb_steps=1000, visualize=False, verbose=2, callbacks=[csv_logger])
-dqn.fit(env, nb_steps=10000, visualize=False, verbose=2)
-scores = dqn.test(env, nb_episodes=10, visualize=True)
-print(np.mean(scores.history['episode_reward']))
+def train_model():
+    dqn = build_agent(model, actions)
+    dqn.compile(Adam(lr=1e-4))
+    callbacks = build_callbacks(ENV_NAME)
+    dqn.fit(env, 
+            nb_steps=NUM_STEPS, 
+            visualize=False, 
+            callbacks=callbacks, 
+            verbose=2
+    )
+    dqn.save_weights(INSTANCE_PATH + '\\finished_weights.h5f')
+
+train_model()
+
+# scores = dqn.test(env, nb_episodes=10, visualize=True)
+# print(np.mean(scores.history['episode_reward']))
